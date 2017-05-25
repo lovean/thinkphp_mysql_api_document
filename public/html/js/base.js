c8f08b99d923235865d87b65c5869b691d734887
Administrator <281689334@qq,com>
2017-05-25 15:27:21 +0800

    vue写了几个界面

--- public/html/js/base.js
+++ public/html/js/base.js
@@ -1,2 +1,3 @@
 var baseUrl = "http://localhost:8888/";
 var loginUrl = baseUrl+"index/Index/login";
+var getApiUrl = baseUrl+"index/User/getApi";
--- public/html/js/index.js
+++ public/html/js/index.js
@@ -1,13 +1,18 @@
 var vm = new Vue({
     el:'#main',
     data :{
-        password:'12345'
+        password:'12345',
+        projectList:[
+        ],
     },
     methods:{
         login:function (event) {
             axios.post(loginUrl,{password:this.password})
                 .then(function(response){
-                    
+                    vm.$data.projectList = [
+                        {'id':1,'name':'停车场项目'},
+                        {'id':2,'name':'聊天室项目'}
+                    ];
                 })
                 .catch(function(err){
 
